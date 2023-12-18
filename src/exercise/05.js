c41c969d749b2bc1e47a8e3f8c344ba60253d2f6
vlaktron <vlaktron@outlook.com>
2023-12-17 23:07:55 -0700

    done

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,19 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+function Box({size, style, ...otherProps}) {
+  return (
+    <div 
+      className={`Box box--${size}`} 
+      style={{fontStyle: 'italic', ...style}} 
+      {...otherProps}
+    />
+  )
+}
+
+const smallBox = <Box size='small' style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
+const mediumBox = <Box size='medium' style={{backgroundColor: 'pink'}}>medium pink box</Box>
+const largeBox = <Box size='large' style={{backgroundColor: 'orange'}}>large orange box</Box>
 
 function App() {
   return (
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,12 +4,17 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameValue = React.useRef()
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
+  function handleSubmit(event) {
+    event.preventDefault()
+    const value = usernameValue.current.value
+    onSubmitUsername(value)
+  }
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
@@ -20,10 +25,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor='usernameInput'>Username:</label>
+        <input ref={usernameValue} id='usernameInput' type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
